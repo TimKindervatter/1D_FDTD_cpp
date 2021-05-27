09d609a9d55c03ed0326c6231c88fc7721ab1d47
Tim <kindert1@tcnj.edu>
2021-05-26 20:55:19 -0400

    Added timing loop to main()

--- 1D_FDTD.cpp
+++ 1D_FDTD.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <chrono>
 
+#include "Eigen\Dense"
+
 #include "FDTD_engine.h"
 #include "Utilities.h"
 
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    FDTD_engine();
-    auto stop = std::chrono::high_resolution_clock::now();
+    constexpr int SIZE = 10;
 
-    auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
-    floating_point_t duration_seconds = 1e-9f * duration_ns;
-    std::cout << duration_seconds;
+    Eigen::Array<double, 1, SIZE> times;
+
+    for (int i = 0; i < SIZE; ++i)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        FDTD_engine();
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+        floating_point_t duration_seconds = 1e-9f * duration_ns;
+
+        times[i] = duration_seconds;
+
+        std::cout << "Run " << i << ": " << duration_seconds << " seconds\n";
+    }
+
+    std::cout << "Average over " << SIZE << " runs: " << times.mean() << " seconds\n";
 
     return 0;
 }
