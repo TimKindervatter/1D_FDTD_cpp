a8e952e98c78e20ad445bf6ec9e4482aac6a4b8f
Tim <kindert1@tcnj.edu>
2021-05-29 00:55:00 -0400

    Added templated function for doing openMP parallelization with any binary operation

--- FDTD_engine.h
+++ FDTD_engine.h
@@ -3,7 +3,7 @@
 #include "Utilities.h"
 
 template <typename InArrayType, typename OutArrayType, typename Callable>
-void parallelize_unary_array_operation(Eigen::Array<InArrayType, 1, -1>& input_array, Eigen::Array<OutArrayType, 1, -1>& output_array, Callable f)
+void parallelize_unary_array_operation(InArrayType& input_array, OutArrayType& output_array, Callable f)
 {
 #pragma omp parallel
 	{
@@ -24,6 +24,32 @@ void parallelize_unary_array_operation(Eigen::Array<InArrayType, 1, -1>& input_a
 		if (start < num_elements)
 		{
 			output_array.segment(start, length) = f(input_array.segment(start, length));
+		}
+	}
+}
+
+template <typename InArray1Type, typename InArray2Type, typename OutArrayType, typename Callable>
+void parallelize_binary_array_operation(InArray1Type& input_array1, InArray2Type& input_array2, OutArrayType& output_array, Callable f)
+{
+#pragma omp parallel
+	{
+		int num_threads = Eigen::nbThreads();
+		int thread_ID = omp_get_thread_num();
+		int num_elements = input_array1.size();
+		int elements_per_thread = num_elements / num_threads;
+
+		if (elements_per_thread * num_threads < num_elements)
+			elements_per_thread++;
+
+		int start = thread_ID * elements_per_thread;
+
+		int length = elements_per_thread;
+		if (thread_ID + 1 == num_threads)
+			length = num_elements - start;
+
+		if (start < num_elements)
+		{
+			output_array.segment(start, length) = f(input_array1.segment(start, length), input_array2.segment(start, length));
 		}
 	}
 }
