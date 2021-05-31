2df536677bb752934b87f0713e776c4546c2a5ac
Tim <kindert1@tcnj.edu>
2021-05-30 23:20:22 -0400

    Changed some types to prevent compiler warnings

--- FDTD_engine.h
+++ FDTD_engine.h
@@ -12,15 +12,15 @@ void parallelize_unary_array_operation(InArrayType& input_array, OutArrayType& o
 	{
 		int num_threads = Eigen::nbThreads();
 		int thread_ID = omp_get_thread_num();
-		int num_elements = input_array.size();
-		int elements_per_thread = num_elements / num_threads;
+		auto num_elements = input_array.size();
+		auto elements_per_thread = num_elements / num_threads;
 
 		if (elements_per_thread * num_threads < num_elements)
 			elements_per_thread++;
 
-		int start = thread_ID * elements_per_thread;
+		auto start = thread_ID * elements_per_thread;
 
-		int length = elements_per_thread;
+		auto length = elements_per_thread;
 		if (thread_ID + 1 == num_threads)
 			length = num_elements - start;
 
